cf497023b60ecee58e9ffba00b00efd526ac0695
nineneel <sjrnl27@gmail.com>
2024-03-08 14:04:31 +0800

    08/02/2024 update revision

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,23 @@
 module.exports = {
   content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
-      extend: {
-          backgroundImage: {
-            'banner': "url('/public/assets/images/banner.webp')",
-            'parenting_educational': "url('/public/assets/images/parenting_educational/background.webp')",
-            'eduall-360': "url('/public/assets/images/eduall-360/background.webp')",
-            'register': "url('/public/assets/images/register/register-banner.webp')"
-          },
-          colors: {
-              primary: "#120FFD",
-              secondary: "#243872",
-              yellow: "#F4EA0F",
-          },
-          fontFamily: {
-              primary: ["Ambit", "sans-serif"],
-          },
+    extend: {
+      backgroundImage: {
+        banner: "url('/public/assets/images/banner.webp')",
+        parenting_educational: "url('/public/assets/images/parenting_educational/background.webp')",
+        "eduall-360": "url('/public/assets/images/eduall-360/background.webp')",
+        register: "url('/public/assets/images/register/register-banner.webp')",
+        "scholarship-item-badge": "url('/public/assets/images/scolarship/item-badge.webp')",
       },
+      colors: {
+        primary: "#120FFD",
+        secondary: "#243872",
+        yellow: "#F4EA0F",
+      },
+      fontFamily: {
+        primary: ["Ambit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
